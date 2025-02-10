c2c5368c752cc182861c04ba5b50dd73470cb3a1
mennaelhosiny <mennaelhosiny3@gmail.com>
2025-02-10 16:06:16 +0200

    updated

--- main.js
+++ main.js
@@ -2,14 +2,9 @@ function toggleMenu() {
     document.querySelector(".nav-links").classList.toggle("open");
 }
 
-
 document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-        if (targetElement) {
-            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
+    link.addEventListener('click', function () {
+        document.querySelector(".nav-links").classList.remove("open");
     });
 });
+
