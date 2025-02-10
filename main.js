4d38a3a9f84c3031560a00af0c3ce08e788ea7b8
mennaelhosiny <mennaelhosiny3@gmail.com>
2025-02-10 13:16:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("open");
+}
+
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
